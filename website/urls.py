a401b90f55973a39cf52f116d25c2bca729ede65
Mohammed Naseer <mnaseer8623@gmail.com>
2021-03-04 02:09:06 -0600

    tweaked urls file for heroku

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('home.html', views.home, name="home"),
+	path('', views.home, name="home"),
 	path('contact.html', views.contact, name="contact"),
 	path('pricing.html', views.pricing, name="pricing"),
 	path('about.html', views.about, name="about"),
